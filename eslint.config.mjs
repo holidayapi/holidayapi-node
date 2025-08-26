3ffe5294da6d9637a05c1dcd4f4d441c3af85053
Josh Sherman <joshsherman@gmail.com>
2025-08-25 21:09:45 -0500

    chore: upgrade to eslint 9

---
+++ eslint.config.mjs
@@ -0,0 +1,24 @@
+import tseslint from '@typescript-eslint/eslint-plugin';
+import tsparser from '@typescript-eslint/parser';
+
+export default [
+  {
+    ignores: ['node_modules/**', 'dist/**', 'coverage/**'],
+  },
+  {
+    files: ['**/*.{js,ts}'],
+    languageOptions: {
+      parser: tsparser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tseslint,
+    },
+    rules: {
+      'no-unused-vars': 'off',
+    },
+  },
+];
