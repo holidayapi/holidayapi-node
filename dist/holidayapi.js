5b335db012017ff522d573cdc90524150b4514f7
Josh Sherman <joshsherman@gmail.com>
2025-08-27 09:43:06 -0500

    feat: update dist build

--- dist/holidayapi.js
+++ dist/holidayapi.js
@@ -20,8 +20,8 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
     });
 };
 var __generator = (this && this.__generator) || function (thisArg, body) {
-    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
-    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g = Object.create((typeof Iterator === "function" ? Iterator : Object).prototype);
+    return g.next = verb(0), g["throw"] = verb(1), g["return"] = verb(2), typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
     function verb(n) { return function (v) { return step([n, v]); }; }
     function step(op) {
         if (f) throw new TypeError("Generator is already executing.");
@@ -74,21 +74,21 @@ var HolidayAPI = (function () {
     };
     HolidayAPI.prototype.request = function (endpoint, request) {
         return __awaiter(this, void 0, void 0, function () {
-            var response, payload, err_1;
-            return __generator(this, function (_a) {
-                switch (_a.label) {
+            var response, payload, _a;
+            return __generator(this, function (_b) {
+                switch (_b.label) {
                     case 0: return [4, (0, node_fetch_1.default)(this.createUrl(endpoint, request))];
                     case 1:
-                        response = _a.sent();
-                        _a.label = 2;
+                        response = _b.sent();
+                        _b.label = 2;
                     case 2:
-                        _a.trys.push([2, 4, , 5]);
+                        _b.trys.push([2, 4, , 5]);
                         return [4, response.json()];
                     case 3:
-                        payload = _a.sent();
+                        payload = _b.sent();
                         return [3, 5];
                     case 4:
-                        err_1 = _a.sent();
+                        _a = _b.sent();
                         payload = {};
                         return [3, 5];
                     case 5:
